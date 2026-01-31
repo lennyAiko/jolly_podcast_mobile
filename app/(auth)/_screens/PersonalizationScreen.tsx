5001657f5b405104c4e87fc334ae471c736287bf
Lennox Charles <lennyaiko17@gmail.com>
2026-01-31 17:36:20 +0100

    progress on stuff

--- app/(auth)/_screens/PersonalizationScreen.tsx
+++ app/(auth)/_screens/PersonalizationScreen.tsx
@@ -2,17 +2,53 @@ import { icons, images } from "@/constants";
 import { personalizationData } from "@/constants/data";
 import { useRegistrationData } from "@/store/registrationStore";
 import { ResizeMode } from "expo-av";
-import React from "react";
-import { FlatList, Image, Text, View } from "react-native";
+import React, { useState } from "react";
+import {
+    ActivityIndicator,
+    Image,
+    Text,
+    TouchableOpacity,
+    View,
+} from "react-native";
 
-const PersonlizedView = ({ title }: { title: string }) => (
-  <View className="">
-    <Text>{title}</Text>
-  </View>
+const PersonlizedView = ({
+  title,
+  onPress,
+  isPersonalized,
+}: {
+  title: string;
+  onPress?: () => void;
+  isPersonalized: boolean;
+}) => (
+  <TouchableOpacity
+    onPress={onPress}
+    className={`flex font-nunito flex-row gap-2 items-center px-4 py-2 rounded-2xl border  ${isPersonalized ? "bg-secondary-400 border-secondary-400" : "bg-white border-secondary-400"}`}
+  >
+    <Text
+      className={`${isPersonalized ? "text-white" : "text-secondary-300"}  text-sm font-nunito`}
+    >
+      {title}
+    </Text>
+    <Image
+      source={isPersonalized ? icons.close : icons.add}
+      className={`${isPersonalized ? "size-4 w-[10px]" : "size-3"}`}
+      tintColor={`${isPersonalized ? "#fff" : "#000"}`}
+    />
+  </TouchableOpacity>
 );
 
 const PersonalizationScreen = () => {
   const registrationData = useRegistrationData();
+
+  const [personalizations, setPersonalizations] = useState<string[]>([]);
+
+  const handleSelected = (title: string) => {
+    if (personalizations.includes(title)) {
+      setPersonalizations((prev) => prev.filter((item) => item !== title));
+      return;
+    }
+    setPersonalizations((prev) => [...prev, title]);
+  };
 
   return (
     <View className="w-full h-full">
@@ -27,29 +63,43 @@ const PersonalizationScreen = () => {
         }}
         resizeMode={ResizeMode.COVER}
       />
-      <View className="flex-1 justify-start items-center px-8">
-        <FlatList
-          data={personalizationData}
-          keyExtractor={(item) => `${item.id}`}
-          renderItem={({ item }) => <PersonlizedView title={item.title} />}
-          showsVerticalScrollIndicator={false}
-          ListHeaderComponent={() => (
-            <View className="flex flex-col gap-1 items-start my-2 w-full">
-              <Image
-                source={icons.jollyicon2}
-                className="-mb-8 size-40"
-                resizeMode={ResizeMode.CONTAIN}
-              />
-              <Text className="text-xl text-white text-start font-nunito-bold">
-                Welcome, {registrationData.firstName || "Lennox"}
-              </Text>
-              <Text className="text-xl text-white text-start font-nunito-bold">
-                Personalize your Jolly experience by selecting your top interest
-                and favorite topics.
-              </Text>
-            </View>
+      <View className="px-8">
+        <View className="flex flex-col gap-1 items-start my-2 w-full">
+          <Image
+            source={icons.headphones}
+            className="size-40"
+            resizeMode={ResizeMode.CONTAIN}
+          />
+          <Text className="text-xl text-white text-start font-nunito-bold">
+            Welcome, {registrationData.firstName || "Lennox"}
+          </Text>
+          <Text className="my-2 text-sm text-white text-start font-nunito">
+            Personalize your Jolly experience by selecting your top interest and
+            favorite topics.
+          </Text>
+        </View>
+        <View className="flex flex-row flex-wrap gap-4 justify-center items-center">
+          {personalizationData.map((item) => (
+            <PersonlizedView
+              key={item.id}
+              title={item.title}
+              onPress={handleSelected.bind(null, item.title)}
+              isPersonalized={personalizations.includes(item.title)}
+            />
+          ))}
+        </View>
+
+        <TouchableOpacity
+          onPress={() => {}}
+          disabled={false}
+          className="flex justify-center items-center py-4 my-10 w-full rounded-full bg-secondary-400 font-nunito-bold"
+        >
+          {false ? (
+            <ActivityIndicator className="text-white size-3" />
+          ) : (
+            <Text className="text-white font-nunito-bold">Continue</Text>
           )}
-        />
+        </TouchableOpacity>
       </View>
     </View>
   );
